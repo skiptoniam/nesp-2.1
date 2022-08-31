cca55f55addc45c2e2f81805b26fdc88fa439f47
claudespencer111 <claudespencer111@ijlqal5ix9pyk1.novalocal>
2022-08-31 05:34:53 +0000

    detrended bathy for jac

--- R/02_habitat model/X_detrend-bathymetry.R
+++ R/02_habitat model/X_detrend-bathymetry.R
@@ -1,9 +1,9 @@
 ###
-# Project: ** Add here **
+# Project: NESP 2.1 
 # Data:    Bathymetry Data
-# Task:    Prepare spatial layers for modelling
-# Author:  Kingsley Griffin & Claude Spencer
-# Date:    ** Add here **
+# Task:    Detrend bathymetry
+# Author:  Claude Spencer
+# Date:    August 2022
 ## 
 
 # This script formats bathymetry data and extracts bathymetry derivatives for modelling habitat and fish
@@ -21,43 +21,34 @@ library(stars)
 library(starsExtra)
 
 # Set your study name
-name <- "Jurien-Two-Rocks"                                                      # Change here
+name <- ""                                                                      # Change here
 
 # Set CRS for bathymetry data
 wgscrs <- "+proj=longlat +datum=WGS84 +south"                                   # Latlong projection 
-# sppcrs <- CRS("+proj=utm +zone=49 +south +datum=WGS84 +units=m +no_defs")       # UTM projection - check your UTM Zone!
 
 # This next section uses coarse GA bathymetry, replace if you have better bathymetry data (ie. multibeam or LiDAR)
 # Read in and merge GA coarse bathy tiles from https://ecat.ga.gov.au/geonetwork/srv/eng/catalog.search#/metadata/67703
 cbaths      <- list.files("data/spatial/rasters/raw bathymetry",                # Bathymetry data too large for git stored here
-                     "*tile", full.names = TRUE) 
+                          "*tile", full.names = TRUE) 
 cbathy      <- lapply(cbaths,                                                   # Loads all of the tiles
-                 function(x){read.table(file = x, header = TRUE, sep = ",")})    
+                      function(x){read.table(file = x, header = TRUE, sep = ",")})    
 cbathy      <- do.call("rbind", lapply(cbathy, as.data.frame))                  # Turns the list into a data frame
-cbathy      <- cbathy[cbathy$Z <= 0, ]                         # Get rid of topography data above 0m, general crop to speed life up
-bath_r      <- terra::rast(cbathy)                                            # Convert to a raster
+cbathy      <- cbathy[cbathy$Z <= 0, ]                                          # Get rid of topography data above 0m
+bath_r      <- terra::rast(cbathy)                                              # Convert to a raster
 crs(bath_r) <- wgscrs                                                           # Set the CRS
 plot(bath_r)                                                                    # Plot to check everything looks ok
 
 # Crop the bathymetry to the general study area
-tbath_c <- crop(bath_r, terra::ext(c(114, 115.75,-32, -30)))
-plot(tbath_c)
-fbath_df <- as.data.frame(tbath_c, xy = TRUE)                                   # Convert this to a dataframe
-saveRDS(fbath_df, paste(paste0('data/spatial/rasters/',                         # Save it for use in the next scripts
-                               name), 'ga_bathy.rds', sep = "_")) 
-
-# Calculate TERRA terrain derivatives
-preds <- terra::terrain(tbath_c, neighbors = 8,
-                 v = c("slope", "aspect", "TPI", "TRI", "roughness"))           # Remove here as necessary
-preds <- rast(list(tbath_c, preds))                                             # Stack the derivatives with the bathymetry
+# tbath_c <- crop(bath_r, terra::ext(c(114, 115.75,-32, -30)))                  # No need for cropping when you want all of aus...
+# plot(tbath_c)
 
 # Calculate detrended bathymetry
 zstar <- st_as_stars(tbath_c)                                                   # Convert to a stars object
 detre <- detrend(zstar, parallel = 8)                                           # Detrend bathymetry - This usually runs quite slow!
 detre <- as(object = detre, Class = "SpatRaster")                               # Convert it to a raster
 names(detre) <- c("detrended", "lineartrend")
-preds <- rast(list(preds, detre))                                               # Make a rasterstack
-plot(preds)
+preds <- rast(detre)                                                            # Make a rasterstack 
+plot(preds)                                                                     # Check it 
 
 # Save the output
 preds <- wrap(preds)
