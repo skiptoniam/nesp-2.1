7cbb909e691a82529797eea814558441d3fe9496
marineecology <marineecology@marineecology.novalocal>
2023-03-07 23:01:37 +0000

    write files

--- R/01_MBH-design/2023-03_points-to-cplot_extra points.R
+++ R/01_MBH-design/2023-03_points-to-cplot_extra points.R
@@ -16,14 +16,15 @@ library(gpx)
 library(sf)
 library(nngeo)
 library(GlobalArchive)
+library(stringr)
 source('R/01_MBH-design/functions.R')
 
 mma <- read.csv("data/mbh-design/13011_Cape_Leeuwin_Submerged_Cultural_Heritage_Landscapes_bg.csv") %>%
   ga.clean.names() %>%
   dplyr::mutate(latitude = str_replace_all(.$wgs84.latitude.dm., "\xb0 ", ".")) %>%
-  dplyr::mutate(latitude = str_replace_all(.$latitude, "' S", "")) %>%
-  dplyr::mutate(longitude = str_replace_all(.$wgs84.latitude.dm., "\xb0 ", ".")) %>%
-  dplyr::mutate(longitude = str_replace_all(.$longitude, "' E", "")) %>%
+  dplyr::mutate(latitude = str_replace_all(.$latitude, " S", "")) %>%
+  dplyr::mutate(longitude = str_replace_all(.$wgs84.longitude.dm., "\xb0 ", ".")) %>%
+  dplyr::mutate(longitude = str_replace_all(.$longitude, " E", "")) %>%
   separate(latitude, into = c("lat.hour", "lat.min", "lat.dec"), sep = "\\.") %>%
   dplyr::mutate(lat.dec = ifelse(str_length(.$lat.min) == 2,
                                  signif(as.numeric(lat.dec), digits = 5) %>% str_sub(start = 1, end = 5), 
@@ -44,7 +45,7 @@ mma.arc <- mma %>%
                 longitude = lon.dd + as.numeric(lon.hour),
                 lat.dd = (as.numeric(paste(lat.min, lat.dec, sep = "."))/60),
                 latitude = (lat.dd + as.numeric(lat.hour))*-1) %>%
-  dplyr::select(longitude, latitude, sample.no, proposed.sampling.methods, dab.no) %>%
+  dplyr::select(longitude, latitude, location.id, dimensions, description.of.location, block) %>%
   glimpse()
 
 write.csv(mma.arc, "data/mbh-design/MMA_BOSS_wgs84.csv", row.names = F)
@@ -56,56 +57,14 @@ cplot.mma <- data.frame("mark" = c("mark"),
                              lon = mma$longitude,
                              lat = mma$latitude, 
                              "symbol" = c("Blue Star"),
-                             "ptcode" = mma$sample.no,
+                             "ptcode" = mma$location.id,
                              c(0))
 head(cplot.mma)
-write.table(cplot.mma , "output/MBH-design/Augusta-SwC_2023-03/2023-03_MMA-backscatter.txt", sep = " ", 
+write.table(cplot.mma , "output/MBH-design/Augusta-SwC_2023-03/2023-03_MMA-submerged-landscapes.txt", sep = " ", 
             col.names = FALSE, row.names = FALSE, quote = FALSE)
 
 paste(min(cplot.mma$lat), min(cplot.mma$lon),
       max(cplot.mma$lat),max(cplot.mma$lon), sep = ",") # Add into 'Bounds' - not sure its needed or not 
-
-# SwC BRUVs
-cplot.bruv.swc <- data.frame("mark" = c("mark"),
-                         "PXYCSLM" = c("PXYCSLM"),
-                         DDtolatlon(swc.bruv[, 5:6]),
-                         "symbol" = c("Blue Star"),
-                         "ptcode" = swc.bruv$sample,
-                         c(0))
-head(cplot.bruv.swc)
-write.table(cplot.bruv.swc , "output/MBH-design/Augusta-SwC_2023-03/2023-03_SwC_stereo-BRUVs.txt", sep = " ", 
-            col.names = FALSE, row.names = FALSE, quote = FALSE)
-
-paste(min(cplot.bruv.swc$lat), min(cplot.bruv.swc$lon),
-      max(cplot.bruv.swc$lat),max(cplot.bruv.swc$lon), sep = ",") # Add into 'Bounds' - not sure its needed or not 
-
-# SwC BOSS
-cplot.boss.swc <- data.frame("mark" = c("mark"),
-                             "PXYCSLM" = c("PXYCSLM"),
-                             DDtolatlon(swc.boss[, 5:6]),
-                             "symbol" = c("Black Star"),
-                             "ptcode" = swc.boss$sample,
-                             c(0))
-head(cplot.boss.swc)
-write.table(cplot.boss.swc , "output/MBH-design/Augusta-SwC_2023-03/2023-03_SwC_BOSS.txt", sep = " ", 
-            col.names = FALSE, row.names = FALSE, quote = FALSE)
-
-paste(min(cplot.boss.swc$lat), min(cplot.boss.swc$lon),
-      max(cplot.boss.swc$lat),max(cplot.boss.swc$lon), sep = ",") # Add into 'Bounds' - not sure its needed or not 
-
-# Augusta BOSS
-cplot.boss.aug <- data.frame("mark" = c("mark"),
-                             "PXYCSLM" = c("PXYCSLM"),
-                             DDtolatlon(aug.boss[, 1:2]),
-                             "symbol" = c("Black Star"),
-                             "ptcode" = aug.boss$sample,
-                             c(0))
-head(cplot.boss.aug)
-write.table(cplot.boss.aug , "output/MBH-design/Augusta-SwC_2023-03/2023-03_Augusta_BOSS.txt", sep = " ", 
-            col.names = FALSE, row.names = FALSE, quote = FALSE)
-
-paste(min(cplot.boss.aug$y), min(cplot.boss.aug$x),
-      max(cplot.boss.aug$y), max(cplot.boss.aug$x), sep = ",") # Add into 'Bounds' - not sure its needed or not 
 
 # # couldn't figure out how to get this to work with the quotation marks so I just pasted manually
 # "TMQ CPlot Chart Type 2   ",
