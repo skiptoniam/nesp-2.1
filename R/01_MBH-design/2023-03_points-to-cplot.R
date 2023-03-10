0ee3fe94a9883ec276898a248cf7dd2bd7cb2948
claudehspencer <claudespencer111@gmail.com>
2023-03-10 10:38:44 +0800

    hoepfully points fixed now!

--- R/01_MBH-design/2023-03_points-to-cplot.R
+++ R/01_MBH-design/2023-03_points-to-cplot.R
@@ -7,6 +7,7 @@
 ##
 
 rm(list = ls())
+gc()
 
 library(tidyverse)
 library(rgdal)
@@ -15,6 +16,8 @@ library(sf)
 library(nngeo)
 library(GlobalArchive)
 source('R/01_MBH-design/functions.R')
+
+data <- aug.boss[,1:2]
 
 aug.boss <- read.csv("data/mbh-design/Augusta_BOSS_MBH_wgs84.csv") %>%
   glimpse() # Slightly different naming
@@ -27,20 +30,18 @@ swc.bruv <- read.csv("data/mbh-design/SwC_BRUV_MBH_wgs84.csv") %>%
   #        lat = as.character(lat)) %>%
   glimpse()
 
+# Need to fix this one in the original import - weird data format
 mma <- read_csv("data/mbh-design/SI-1301 Proposed Seabed Sample Locations Cape Leeuwin_Rev1.csv") %>%
   ga.clean.names() %>%
   dplyr::mutate(latitude = str_replace_all(.$latitude, "\xb0 ", ".")) %>%
   dplyr::mutate(latitude = str_replace_all(.$latitude, "' S", "")) %>%
   dplyr::mutate(longitude = str_replace_all(.$longitude, "\xb0 ", ".")) %>%
   dplyr::mutate(longitude = str_replace_all(.$longitude, "' E", "")) %>%
-  separate(latitude, into = c("lat.hour", "lat.min", "lat.dec"), sep = "\\.") %>%
-  dplyr::mutate(lat.dec = ifelse(str_length(.$lat.min) == 2,
-                                 signif(as.numeric(lat.dec), digits = 5) %>% str_sub(start = 1, end = 5), 
-                                 signif(as.numeric(lat.dec), digits = 6) %>% str_sub(start = 1, end = 6))) %>%
-  separate(longitude, into = c("lon.hour", "lon.min", "lon.dec"), sep = "\\.") %>%
-  dplyr::mutate(lon.dec = ifelse(str_length(.$lon.min) == 2,
-                                 signif(as.numeric(lon.dec), digits = 4) %>% str_sub(start = 1, end = 4), 
-                                 signif(as.numeric(lon.dec), digits = 5) %>% str_sub(start = 1, end = 5))) %>%
+  separate(latitude, into = c("lat.hour", "lat.min", "lat.dec"), sep = "\\.", remove = F) %>%
+  dplyr::mutate(lat.dec = str_sub(.$lat.dec, start = 1, end = 4),
+                lat.min = str_pad(.$lat.min, side = "left", pad = "0", width = 2)) %>%
+  separate(longitude, into = c("lon.hour", "lon.min", "lon.dec"), sep = "\\.", remove = F) %>%
+  dplyr::mutate(lon.dec = str_sub(.$lon.dec, start = 1, end = 4)) %>% #signif(as.numeric(lon.dec), digits = 5) %>% 
   dplyr::mutate(latitude = paste0(paste(lat.hour, lat.min, lat.dec, sep = "."), "S"),
                 longitude = paste0(paste(lon.hour, lon.min, lon.dec, sep = "."), "E")) %>%
   glimpse()
@@ -57,6 +58,7 @@ write.csv(mma.arc, "data/mbh-design/MMA_BOSS_wgs84.csv", row.names = F)
 
 # Export to txt file ready for processing into cplot .MRK file
 # MMA samples
+# Already have parsed the lat long format into the required for cplot
 cplot.mma <- data.frame("mark" = c("mark"),
                              "PXYCSLM" = c("PXYCSLM"),
                              lon = mma$longitude,
@@ -73,11 +75,30 @@ paste(min(cplot.mma$lat), min(cplot.mma$lon),
 
 # SwC BRUVs
 cplot.bruv.swc <- data.frame("mark" = c("mark"),
-                         "PXYCSLM" = c("PXYCSLM"),
-                         DDtolatlon(swc.bruv[, 5:6]),
-                         "symbol" = c("Blue Star"),
-                         "ptcode" = swc.bruv$sample,
-                         c(0))
+                             "PXYCSLM" = c("PXYCSLM"),
+                             lon = measurements::conv_unit(swc.bruv$lon, 
+                                                           from = "dec_deg", 
+                                                           to = "deg_dec_min"),
+                             lat = measurements::conv_unit(swc.bruv$lat, 
+                                                           from = "dec_deg", 
+                                                           to = "deg_dec_min"),
+                             "symbol" = c("Green Star"),
+                             "ptcode" = swc.bruv$sample,
+                             c(0)) %>%
+  separate(lon, into = c("lon.hour", "lon.dm"), sep = " ") %>%
+  separate(lat, into = c("lat.hour", "lat.dm"), sep = " ") %>%
+  separate(lon.dm, into = c("lon.min", "lon.dec"), sep = "\\.") %>%
+  separate(lat.dm, into = c("lat.min", "lat.dec"), sep = "\\.") %>%
+  dplyr::mutate(lon.dec = str_trunc(.$lon.dec, width = 4,  side = "right", ellipsis = ""),
+                lon.min = str_pad(.$lon.min, side = "left", pad = "0", width = 2),
+                lat.dec = str_trunc(.$lat.dec, width = 4,  side = "right", ellipsis = ""),
+                lat.min = str_pad(.$lat.min, side = "left", pad = "0", width = 2)) %>%
+  dplyr::mutate(lon.dec = str_trunc(.$lon.dec, width = 4,  side = "right", ellipsis = "")) %>%
+  dplyr::mutate(lon.dec = str_pad(.$lon.dec, side = "right", pad = "0", width = 4),
+                lon = paste0(paste(lon.hour, lon.min, lon.dec, sep = "."), "E"),
+                lat = sub('.', '',paste0(paste(lat.hour, lat.min, lat.dec, sep = "."), "S"))) %>%
+  dplyr::select(mark, PXYCSLM, lon, lat, symbol, ptcode, c.0.) %>%
+  glimpse()
 
 head(cplot.bruv.swc)
 write.table(cplot.bruv.swc , "output/MBH-design/Augusta-SwC_2023-03/2023-03_SwC_stereo-BRUVs.txt", sep = " ", 
@@ -89,10 +110,30 @@ paste(min(cplot.bruv.swc$lat), min(cplot.bruv.swc$lon),
 # SwC BOSS
 cplot.boss.swc <- data.frame("mark" = c("mark"),
                              "PXYCSLM" = c("PXYCSLM"),
-                             DDtolatlon(swc.boss[, 5:6]),
-                             "symbol" = c("Black Star"),
+                             lon = measurements::conv_unit(swc.boss$lon, 
+                                                           from = "dec_deg", 
+                                                           to = "deg_dec_min"),
+                             lat = measurements::conv_unit(swc.boss$lat, 
+                                                           from = "dec_deg", 
+                                                           to = "deg_dec_min"),
+                             "symbol" = c("Green Star"),
                              "ptcode" = swc.boss$sample,
-                             c(0))
+                             c(0)) %>%
+  separate(lon, into = c("lon.hour", "lon.dm"), sep = " ") %>%
+  separate(lat, into = c("lat.hour", "lat.dm"), sep = " ") %>%
+  separate(lon.dm, into = c("lon.min", "lon.dec"), sep = "\\.") %>%
+  separate(lat.dm, into = c("lat.min", "lat.dec"), sep = "\\.") %>%
+  dplyr::mutate(lon.dec = str_trunc(.$lon.dec, width = 4,  side = "right", ellipsis = ""),
+                lon.min = str_pad(.$lon.min, side = "left", pad = "0", width = 2),
+                lat.dec = str_trunc(.$lat.dec, width = 4,  side = "right", ellipsis = ""),
+                lat.min = str_pad(.$lat.min, side = "left", pad = "0", width = 2)) %>%
+  dplyr::mutate(lon.dec = str_trunc(.$lon.dec, width = 4,  side = "right", ellipsis = "")) %>%
+  dplyr::mutate(lon.dec = str_pad(.$lon.dec, side = "right", pad = "0", width = 4),
+                lon = paste0(paste(lon.hour, lon.min, lon.dec, sep = "."), "E"),
+                lat = sub('.', '',paste0(paste(lat.hour, lat.min, lat.dec, sep = "."), "S"))) %>%
+  dplyr::select(mark, PXYCSLM, lon, lat, symbol, ptcode, c.0.) %>%
+  glimpse()
+
 head(cplot.boss.swc)
 write.table(cplot.boss.swc , "output/MBH-design/Augusta-SwC_2023-03/2023-03_SwC_BOSS.txt", sep = " ", 
             col.names = FALSE, row.names = FALSE, quote = FALSE)
@@ -101,33 +142,6 @@ paste(min(cplot.boss.swc$lat), min(cplot.boss.swc$lon),
       max(cplot.boss.swc$lat),max(cplot.boss.swc$lon), sep = ",") # Add into 'Bounds' - not sure its needed or not 
 
 # Augusta BOSS
-cplot.boss.aug.commas <- data.frame("mark" = c("mark"),
-                             "PXYCSLM" = c("PXYCSLM"),
-                             lon = measurements::conv_unit(aug.boss$x, 
-                                                           from = "dec_deg", 
-                                                           to = "deg_dec_min"),
-                             lat = measurements::conv_unit(aug.boss$y, 
-                                                           from = "dec_deg", 
-                                                           to = "deg_dec_min"),
-                             # DDtolatlon(aug.boss[,1:2]),
-                             "symbol" = c("Green Diamond"),
-                             "ptcode" = aug.boss$sample,
-                             c(0)) %>%
-  separate(lon, into = c("lon.hour", "lon.dm"), sep = " ") %>%
-  separate(lat, into = c("lat.hour", "lat.dm"), sep = " ") %>%
-  dplyr::mutate(lon.dm = as.character(signif(as.numeric(lon.dm, digits = 6))),
-                lat.dm = as.character(signif(as.numeric(lat.dm, digits = 6)))) %>%
-  separate(lon.dm, into = c("lon.min", "lon.dec"), sep = "\\.") %>%
-  separate(lat.dm, into = c("lat.min", "lat.dec"), sep = "\\.") %>%
-  dplyr::mutate(lon.dec = as.character(signif(as.numeric(lon.dec, digits = 4))),
-                lon.min = str_pad(.$lon.min, side = "left", pad = "0", width = 2)) %>%
-  dplyr::mutate(lon.dec = str_trunc(.$lon.dec, width = 4,  side = "right", ellipsis = "")) %>%
-  dplyr::mutate(lon.dec = str_pad(.$lon.dec, side = "right", pad = "0", width = 4),
-                lon = paste0(paste(lon.hour, lon.min, lon.dec, sep = "."), ",E"),
-                lat = sub('.', '',paste0(paste(lat.hour, lat.min, lat.dec, sep = "."), ",S"))) %>%
-  dplyr::select(mark, PXYCSLM, lon, lat, symbol, ptcode, c.0.) %>%
-  glimpse()
-
 cplot.boss.aug <- data.frame("mark" = c("mark"),
                              "PXYCSLM" = c("PXYCSLM"),
                              lon = measurements::conv_unit(aug.boss$x, 
@@ -136,18 +150,17 @@ cplot.boss.aug <- data.frame("mark" = c("mark"),
                              lat = measurements::conv_unit(aug.boss$y, 
                                                            from = "dec_deg", 
                                                            to = "deg_dec_min"),
-                             # DDtolatlon(aug.boss[,1:2]),
-                             "symbol" = c("Green Diamond"),
+                             "symbol" = c("Green Star"),
                              "ptcode" = aug.boss$sample,
                              c(0)) %>%
   separate(lon, into = c("lon.hour", "lon.dm"), sep = " ") %>%
   separate(lat, into = c("lat.hour", "lat.dm"), sep = " ") %>%
-  dplyr::mutate(lon.dm = as.character(signif(as.numeric(lon.dm, digits = 6))),
-                lat.dm = as.character(signif(as.numeric(lat.dm, digits = 6)))) %>%
   separate(lon.dm, into = c("lon.min", "lon.dec"), sep = "\\.") %>%
   separate(lat.dm, into = c("lat.min", "lat.dec"), sep = "\\.") %>%
-  dplyr::mutate(lon.dec = as.character(signif(as.numeric(lon.dec, digits = 4))),
-                lon.min = str_pad(.$lon.min, side = "left", pad = "0", width = 2)) %>%
+  dplyr::mutate(lon.dec = str_trunc(.$lon.dec, width = 4,  side = "right", ellipsis = ""),
+                lon.min = str_pad(.$lon.min, side = "left", pad = "0", width = 2),
+                lat.dec = str_trunc(.$lat.dec, width = 4,  side = "right", ellipsis = ""),
+                lat.min = str_pad(.$lat.min, side = "left", pad = "0", width = 2)) %>%
   dplyr::mutate(lon.dec = str_trunc(.$lon.dec, width = 4,  side = "right", ellipsis = "")) %>%
   dplyr::mutate(lon.dec = str_pad(.$lon.dec, side = "right", pad = "0", width = 4),
                 lon = paste0(paste(lon.hour, lon.min, lon.dec, sep = "."), "E"),
@@ -155,12 +168,7 @@ cplot.boss.aug <- data.frame("mark" = c("mark"),
   dplyr::select(mark, PXYCSLM, lon, lat, symbol, ptcode, c.0.) %>%
   glimpse()
 
-DDtolatlon(aug.boss[66,1:2])
-DDtolatlon(aug.boss[81,1:2])
-
 write.table(cplot.boss.aug , "output/MBH-design/Augusta-SwC_2023-03/2023-03_Augusta_BOSS_20230310.txt", sep = " ", 
-            col.names = FALSE, row.names = FALSE, quote = FALSE)
-write.table(cplot.boss.aug.commas , "output/MBH-design/Augusta-SwC_2023-03/2023-03_Augusta_BOSS_20230310-commas.txt", sep = " ", 
             col.names = FALSE, row.names = FALSE, quote = FALSE)
 
 paste(min(cplot.boss.aug$lat), min(cplot.boss.aug$lon),
