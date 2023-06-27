427cf8794c7044a7a5ed1ae77e5849558ce23e1c
claudehspencer <claudespencer111@gmail.com>
2023-06-28 07:44:53 +0800

    making a plot for habtiat mapping 2 pager

---
+++ R/01_MBH-design/plot_design.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(ggplot2)
+library(sf)
+library(terra)
+library(ggnewscale)
+library(scales)
+library(viridis)
+
+e <- ext(114.9, 115.8, -33.7,-33.2)
+
+dat <- st_read("data/mbh-design/National_Sampling_Master_WGS84.shp") %>%
+  st_crop(e) %>%
+  glimpse()
+
+aus <- st_read("data/spatial/shapefiles/cstauscd_r.mif", crs = 4283) %>%
+  dplyr::filter(!FEAT_CODE %in% "sea") %>%
+  st_crop(e) %>%
+  glimpse()
+
+bathy <- rast("data/spatial/rasters/raw bathymetry/bath_250_good.tif") %>%
+  crop(e) %>%
+  as.data.frame(xy = TRUE)
+
+p1 <- ggplot() +
+  geom_raster(data = bathy, aes(x = x, y = y, fill = bath_250_good),
+              show.legend = F, alpha = 0.8) +
+  scale_fill_viridis() +
+  new_scale_fill() +
+  geom_sf(data = aus, fill = "seashell2", colour = "grey80", size = 5) +
+  geom_sf(data = dat, shape = 4) +
+  annotate(geom = "point", x = 115.6409, y = -33.3270) + # Bunbury
+  annotate(geom = "point", x = 115.3473, y = -33.6516) + # Busselton
+  annotate(geom = "text", x = c(115.6409 + 0.03, 115.3473 + 0.035), 
+           y = c(-33.3270, -33.6516), label = c("Bunbury", "Busselton"),
+           fontface = "italic", size = 3) +
+  labs(x = NULL, y = NULL) +
+  coord_sf(xlim = c(115, 115.7), ylim = c(-33.65, -33.299)) +
+  theme_minimal()
+  
+png(filename = "plots/geographe-mbh-exerpt.png", height = 4.5, width = 8,
+      units = "in", res = 300)
+p1
+dev.off()
+
+
